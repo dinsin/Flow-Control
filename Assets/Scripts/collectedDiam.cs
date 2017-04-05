7bd2b787399128d6b5ce9fd59a49bd5351ab8d7f
Laila Esa <lae280@nyu.edu>
2017-04-05 15:24:05 -0400

    most updated version

---
+++ Assets/Scripts/collectedDiam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectedDiam : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if(col.gameObject.tag == "player"){
+			GetComponent<AudioSource> ().Play();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
