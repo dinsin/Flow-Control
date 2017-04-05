3143bf60f7afd642980d7384a2adf61cf4fc0ea5
Dinesh Singh <dinesh.singh@nyu.edu>
2017-04-05 14:34:48 -0400

    Minor changes and beginning to make level select

---
+++ Assets/Scripts/levelSelection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelSelection : MonoBehaviour {
+
+	public GameObject indicator;
+	public GameObject[] levels;
+
+	void Start(){
+		indicator.transform.position = -levels[0].transform.up;
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		//indicator.transform.position += new Vector3(1f, 0f, 0f);
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			SceneManager.LoadScene ("LevelSelect");
+		}
+
+
+	}
+}
