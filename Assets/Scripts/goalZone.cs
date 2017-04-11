f6a750da20dc7fbd975ee96ccc6df20e988bb808
Laila Esa <lae280@nyu.edu>
2017-04-10 21:09:53 -0400

    added pause after level is completed. Placed 'usingObst' level after level1.3

--- Assets/Scripts/goalZone.cs
+++ Assets/Scripts/goalZone.cs
@@ -13,22 +13,30 @@ public class goalZone : MonoBehaviour {
 		
 	}
 
+	IEnumerator nextLevel(){
+		yield return new WaitForSeconds (.5f);
+		if (SceneManager.GetActiveScene ().name  == "Level1.1")
+			SceneManager.LoadScene ("Level1.2");
+		else if (SceneManager.GetActiveScene ().name  == "Level1.2")
+			SceneManager.LoadScene ("Level1.3");
+		//else if (SceneManager.GetActiveScene ().name == "Level1.3")
+		//	SceneManager.LoadScene ("Proto2");
+		else if (SceneManager.GetActiveScene ().name == "Level1.3")
+			SceneManager.LoadScene ("usingObst");
+
+		else if (SceneManager.GetActiveScene ().name == "usingObst")
+			SceneManager.LoadScene ("Level2");
+
+		else if (SceneManager.GetActiveScene ().name == "Level2")
+			SceneManager.LoadScene ("Level3");
+	}
+
 
 	void OnTriggerEnter2D(Collider2D col){
 		if(col.gameObject.tag == "player"){
 			player.GetComponent<playerMovement> ().goal = true;
 			aS.GetComponent<gameSound> ().goalReached = true;
-	
-			if (SceneManager.GetActiveScene ().name  == "Level1.1")
-				SceneManager.LoadScene ("Level1.2");
-			else if (SceneManager.GetActiveScene ().name  == "Level1.2")
-				SceneManager.LoadScene ("Level1.3");
-			//else if (SceneManager.GetActiveScene ().name == "Level1.3")
-			//	SceneManager.LoadScene ("Proto2");
-			else if (SceneManager.GetActiveScene ().name == "Level1.3")
-				SceneManager.LoadScene ("Level2");
-			else if (SceneManager.GetActiveScene ().name == "Level2")
-				SceneManager.LoadScene ("Level3");
+			StartCoroutine (nextLevel ());
 
 		}
 	}
