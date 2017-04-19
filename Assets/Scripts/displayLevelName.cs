d228bff2c3b0c90007f338e4b9519f4e660e1737
Dinesh Singh <dinesh.singh@nyu.edu>
2017-04-19 00:52:50 -0400

    Simple restart to help with presentation. ESC once in any level to go to level select, ESC again to go to title

---
+++ Assets/Scripts/displayLevelName.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class displayLevelName : MonoBehaviour {
+
+	public GameObject overlay;
+	public Text levelName;
+
+	// Use this for initialization
+	void Start () {
+		//overlay.SetActive (true);
+		//levelName.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		StartCoroutine (EnableOverlay ());
+		StartCoroutine (DisableOverlay ());
+	}
+
+	IEnumerator DisableOverlay(){
+		yield return new WaitForSeconds (2.4f);
+		overlay.SetActive (false);
+		levelName.enabled = false;
+	}
+
+	IEnumerator EnableOverlay(){
+		overlay.SetActive (true);
+		levelName.enabled = true;
+		yield return new WaitForSeconds (.1f);
+	}
+}
---
+++ Assets/Scripts/restart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.GetActiveScene ().name != "Title" && SceneManager.GetActiveScene ().name != "LevelSelect"){
+			SceneManager.LoadScene ("LevelSelect");
+		}
+		else if(Input.GetKeyDown(KeyCode.Escape) && SceneManager.GetActiveScene ().name == "LevelSelect"){
+			SceneManager.LoadScene ("Title");
+		}
+	}
+}
